219ebfc9bb99ca69008051c953386a2053f24a17
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-20 18:12:26 +0100

    sixth snack complete

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,13 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const zucchineLunghe = zucchine.filter(function(zucchina) {
+  return zucchina.length >= 15;
+});
+
+const zucchineNonLunghe = zucchine.filter(function(zucchina) {
+  return zucchina.length < 15;
+});
+
+console.log(zucchineLunghe, zucchineNonLunghe);
