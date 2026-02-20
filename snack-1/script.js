3c46316cbf7fbebeb01795bace24e326a925a098
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-20 18:43:40 +0100

    project complete (fix map)

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,10 +3,17 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-let nameArr = [];
+/* let nameArr = [];
 
 names.forEach(function (name) {
     nameArr.push(`'${name}'`);
 });
 
-console.log(nameArr.join(', '));
+console.log(nameArr.join(', ')); */
+
+const namePeople = names.map(function(name) {
+    return name;
+});
+
+console.log(namePeople);
+
--- snack-2/script.js
+++ snack-2/script.js
@@ -7,24 +7,35 @@ const people = [
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
 
+
+
 /* let namePeople = ''
 for (let i = 0; i < people.length; i++) {
   const element = people[i];
 //console.log(element.name); 
   
 
-let simoncino = element.name
-let space = `'${simoncino}' `
+let obj = element.name
+let space = `'${obj}' `
 
 namePeople += space
 }
 console.log(namePeople); */
 
-let namePeople = [];
+
+
+/* let namePeople = [];
 
 people.forEach(function (obj) {
   namePeople.push(`${obj.name}`)
   console.log(obj.name);
 
 });
-console.log(namePeople.join(', '));
+console.log(namePeople.join(', ')); */
+
+
+const namePeople = people.map(function(name){
+  return name.name
+})
+
+console.log(namePeople);
