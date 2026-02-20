a79592a93ac73a6353240a6e537b96841be4ec6a
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-20 17:51:48 +0100

    fourth snack complete

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorsName = posts.map(function (author) {
+  return author.author
+});
+
+console.log(authorsName);
