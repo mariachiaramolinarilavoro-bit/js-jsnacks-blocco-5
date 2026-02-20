292809c92cf0811b781e960152948df0db12facf
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-20 15:11:35 +0100

    first snack complete

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+let nameString = ''
+
+names.forEach(function(name){
+ nameString += `${name} `
+}) 
+console.log(nameString);
